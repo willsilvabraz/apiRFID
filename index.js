8701d924b857c432f36c11f033a23dc88c5bbec4
Daniel Vidal <vidalffyt@gmail.com>
2024-05-22 09:57:07 -0300

    daviBD

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 
 // Inicialize o app do Firebase Admin SDK
-const serviceAccount = require('./hahaha.json');
+const serviceAccount = require('./DaviBD3.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://teste-dfb53-default-rtdb.firebaseio.com"
+  databaseURL: "https://projetoflask-fb-default-rtdb.firebaseio.com"
 });
 
 const db = admin.database();
-const palavrasRef = db.ref("UIDs");
+const palavrasRef = db.ref("cartoes");
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,10 +22,10 @@ app.post('/cadUid', (req, res) => {
     const { uid,  } = req.body;
     if (uid) {
         cadastrarPalavra(uid, "pendente")
-            .then(() => res.status(200).send('UID cadastrado com sucesso!'))
-            .catch(error => res.status(500).send(`Erro ao cadastrar UID: ${error}`));
+            .then(() => res.status(200).send('Cartão cadastrado com sucesso!'))
+            .catch(error => res.status(500).send(`Erro ao cadastrar Cartão: ${error}`));
     } else {
-        res.status(400).send('UID e status são obrigatórios.');
+        res.status(400).send('UID é obrigatório.');
     }
 });
 
