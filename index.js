a842ec66f02b1ff4c1292289839cb415ee912d91
willsilvabraz <willsilvabraz@gmail.com>
2024-05-26 12:49:14 -0300

    asd

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 
 // Inicialize o app do Firebase Admin SDK
-const serviceAccount = require('./DaviBD3.json');
+const serviceAccount = require('./hahaha.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://projetoflask-fb-default-rtdb.firebaseio.com"
+  databaseURL: "https://teste-dfb53-default-rtdb.firebaseio.com"
 });
 
 const db = admin.database();
@@ -21,22 +21,47 @@ app.use(bodyParser.json());
 app.post('/cadUid', (req, res) => {
   const { uid } = req.body;
   if (uid) {
-      // Verificar se o UID já existe
-      palavrasRef.orderByChild("uid").equalTo(uid).once("value", snapshot => {
-          if (snapshot.exists()) {
-              // UID já existe, retornar erro
-              res.status(400).send('UID já cadastrado.');
-          } else {
-              // UID não existe, cadastrar
-              cadCartao(uid)
-                  .then(() => res.status(200).send('Cartão cadastrado com sucesso!'))
-                  .catch(error => res.status(500).send(`Erro ao cadastrar UID: ${error}`));
-          }
-      });
+    // Verificar se o UID já existe
+    palavrasRef.orderByChild("uid").equalTo(uid).once("value", snapshot => {
+      if (snapshot.exists()) {
+        // UID já existe, retornar o status
+        snapshot.forEach(childSnapshot => {
+          const status = childSnapshot.val().status;
+          res.status(200).send(`UID já cadastrado. Status: ${status}`);
+        });
+      } else {
+        // UID não existe, cadastrar
+        cadCartao(uid)
+          .then(() => res.status(200).send('Cartão cadastrado com sucesso!'))
+          .catch(error => res.status(500).send(`Erro ao cadastrar UID: ${error}`));
+      }
+    });
   } else {
-      res.status(400).send('Cartão é obrigatório.');
+    res.status(400).send('Cartão é obrigatório.');
   }
 });
+
+
+// Função para cadastrar o cartão com UID e status "cadastrado"
+function cadCartao(uid) {
+  return new Promise((resolve, reject) => {
+      // Cria um novo objeto com UID e status "cadastrado"
+      const novoCartao = {
+          uid: uid,
+          status: 'cadastrado'
+      };
+
+      // Insere o novo cartão no banco de dados
+      palavrasRef.push(novoCartao, error => {
+          if (error) {
+              reject(error);
+          } else {
+              resolve();
+          }
+      });
+  });
+}
+
 
 
 const PORT = process.env.PORT || 3000;
