ca11217a9b04bac968c13e84e81fabcb0703cb29
willsilvabraz <willsilvabraz@gmail.com>
2024-05-26 12:33:27 -0300

    ok

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.use(bodyParser.json());
 
 // Rota para cadastrar UID
 app.post('/cadUid', (req, res) => {
-<<<<<<< HEAD
   const { uid } = req.body;
   if (uid) {
       // Verificar se o UID já existe
@@ -40,23 +39,6 @@ app.post('/cadUid', (req, res) => {
 });
 
 
-=======
-    const { uid,  } = req.body;
-    if (uid) {
-        cadastrarPalavra(uid, "pendente")
-            .then(() => res.status(200).send('Cartão cadastrado com sucesso!!!'))
-            .catch(error => res.status(500).send(`Erro ao cadastrar Cartão: ${error}`));
-    } else {
-        res.status(400).send('UID é obrigatório.');
-    }
-});
-
-function cadastrarPalavra(uid, status) {
-  return palavrasRef.push({ uid, status });
-}
-
-// Iniciar o servidor
->>>>>>> a9c7badcb4122b253f99ee9753232d4959576655
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
